c43a26185b29e6532b64055145450aac49b306d6
Magnaldo Melo <magnado.melo@gmail.coom>
2023-04-04 10:25:33 -0300

    Removing redis

--- src/main/java/com/southsystem/challengebackvote/domain/model/internal/Section.java
+++ src/main/java/com/southsystem/challengebackvote/domain/model/internal/Section.java
@@ -1,18 +1,11 @@
 package com.southsystem.challengebackvote.domain.model.internal;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.Instant;
 
@@ -27,10 +20,6 @@ public class Section {
     private String id;
     private Agenda agenda;
     private Integer minutesToFinish;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
-    @JsonSerialize(using = InstantSerializer.class)
-    @JsonDeserialize(using = InstantDeserializer.class)
     private Instant expiration;
     private Boolean cloused;
 }
